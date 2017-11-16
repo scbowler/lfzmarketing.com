cb26b0ab06a2c626e074abb08a1062d208e1e9e4
Scott Bowler <scott.bowler@learningfuze.com>
2017-11-16 11:30:56 -0800

    Started adding auth

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,6 +5,12 @@ export function getStudentList(){
     return dispatch => {
         axios.get('/api/student-list').then( resp => {
 
+            if(!resp.data.auth){
+                dispatch({
+                    type: 'NOT_AUTH'
+                });
+                return;
+            }
             dispatch({
                 type: types.GET_STUDENT_LIST,
                 payload: resp.data
@@ -18,6 +24,13 @@ export function getStudentList(){
 export function getMarketingData(){
     return dispatch => {
         axios.get('/api/marketing-data').then( resp => {
+
+            if(!resp.data.auth){
+                dispatch({
+                    type: 'NOT_AUTH'
+                });
+                return;
+            }
 
             dispatch({
                 type: types.GET_MARKETING_DATA,
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import marketing from './marketing_reducer';
 import students from './student_reducer';
+import user from './user_reducer';
 
-export default combineReducers({ marketing, students });
+export default combineReducers({ marketing, students, user });
